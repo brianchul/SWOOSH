e18c0ff69a468d091dd757e89a0ad42d0c757b64
Angelababy <lily8681520@gms.tku.edu.tw>
2018-11-28 22:55:18 +0800

    order

--- vue/src/components/Order.vue
+++ vue/src/components/Order.vue
@@ -1,6 +1,11 @@
 <template>
     <div>
         Order
+        <el-table :data="" class="tbTitle">
+          <el-table-column label="日期"></el-table-column>
+          <el-table-column label="重量"></el-table-column>
+          <el-table-column label="高度"></el-table-column>
+        </el-table>
     </div>
 </template>
 
@@ -11,5 +16,8 @@ export default {
 </script>
 
 <style scoped>
-    
+    .tbTitle{
+        height: 200px;
+        border-radius: 4px;
+    }
 </style>
