31c2433cf2c3a5351a12c0cfc4358687d5e4d200
Angelababy <lily8681520@gms.tku.edu.tw>
2018-12-20 01:11:02 +0800

    user訂單管理編輯

--- vue/src/components/Order.vue
+++ vue/src/components/Order.vue
@@ -2,7 +2,7 @@
   <div id="order">
     <div class="title">訂單管理</div>
     <template v-if="userInfo.permission === 'user'">
-      <el-table class="tbTitle" :data="needData" height="380">
+      <el-table class="tbTitle" :data="showneedData" height="380" key="Need">
         <el-table-column type="expand">
           <template slot-scope="nprops">
             <el-form inline class="table-expand">
@@ -32,11 +32,78 @@
             >{{scope.row.status ? "開放中" : "已關閉"}}</el-tag>
           </template>
         </el-table-column>
+        <el-table-column>
+          <template slot-scope="scope"><el-tooltip class="item" effect="dark" content="編輯" placement="right">
+            <el-button
+              icon="el-icon-edit"
+              type="info"
+              circle
+              @click="showNeedOverlay(scope.$index)"
+            ></el-button></el-tooltip>
+          </template>
+        </el-table-column>
       </el-table>
+      <el-dialog title="訂單編輯" :visible.sync="NisActive">
+        <el-form :model="selectedNeedlist" :label-position="right" label-width="90px">
+          <el-form-item label="衛星名稱：">
+            <el-input v-model="selectedNeedlist.name" autocomplete="off" clearable></el-input>
+          </el-form-item>
+          <el-form-item label="發射高度：">
+            <el-select v-model="selectedNeedlist.height" placeholder="請選擇軌道高度">
+              <el-option label="Low-Earth Orbit" value="LEO"></el-option>
+              <el-option label="Medium-Earth Orbit" value="MEO"></el-option>
+              <el-option label="Geostationary Orbit" value="GEO"></el-option>
+            </el-select>
+          </el-form-item>
+          <el-form-item label="發射傾角：">
+            <el-select v-model="selectedNeedlist.inclination" placeholder="請選擇發射傾角">
+              <el-option label="5~14度" value="5~14"></el-option>
+              <el-option label="15~24度" value="15~24"></el-option>
+              <el-option label="25~34度" value="25~34"></el-option>
+              <el-option label="35~44度" value="35~44"></el-option>
+              <el-option label="45~54度" value="45~54"></el-option>
+              <el-option label="55~64度" value="55~64"></el-option>
+              <el-option label="65~74度" value="65~74"></el-option>
+              <el-option label="75~84度" value="75~84"></el-option>
+            </el-select>
+          </el-form-item>
+          <el-form-item label="衛星重量：">
+            <el-input v-model="selectedNeedlist.weight" autocomplete="off" clearable></el-input>
+          </el-form-item>
+          <el-form-item label="發射日期：">
+            <el-date-picker v-model="selectedNeedlist.launchDate" type="date" placeholder="請選擇日期"></el-date-picker>
+          </el-form-item>
+          <el-form-item label="進場日期：">
+            <el-date-picker
+              v-model="selectedNeedlist.toFactoryDate"
+              type="date"
+              placeholder="請選擇日期"
+            ></el-date-picker>
+          </el-form-item>
+          <el-form-item label="預算：">
+            <el-input v-model="selectedNeedlist.budget" autocomplete="off" clearable></el-input>
+          </el-form-item>
+          <el-form-item label="此次目的：">
+            <el-input
+              type="textarea"
+              v-model="selectedNeedlist.purpose"
+              autocomplete="off"
+              clearable
+            ></el-input>
+          </el-form-item>
+          <el-form-item label="狀態：">
+            <el-switch v-model="selectedNeedlist.status" active-text="開放中" inactive-text="關閉"></el-switch>
+          </el-form-item>
+        </el-form>
+        <div slot="footer" class="dialog-footer">
+          <el-button @click="changeNeedOverlay">取消</el-button>
+          <el-button type="primary" @click="modify_N(selectedNeedlist)">儲存</el-button>
+        </div>
+      </el-dialog>
     </template>
     <template v-else>
       <div class="title2">。火箭</div>
-      <el-table class="tbTitle" :data="showrocketData">
+      <el-table class="tbTitle" :data="showrocketData" key="Rocket">
         <el-table-column type="expand">
           <template slot-scope="props">
             <el-form inline class="table-expand">
@@ -64,13 +131,12 @@
         </el-table-column>
         <el-table-column>
           <template slot-scope="scope">
-            <el-button size="mini" @click="showOverlay(scope.$index)">編輯</el-button>
-            <!-- <el-button size="mini" type="danger" @click="open3(scope.$index, scope.row)">刪除</el-button> -->
+            <el-button icon="el-icon-edit" type="info" circle @click="showOverlay(scope.$index)"></el-button>
           </template>
         </el-table-column>
       </el-table>
       <div class="title2">。機位</div>
-      <el-table class="tbTitle" :data="showsaleData">
+      <el-table class="tbTitle" :data="showsaleData" key="Sale">
         <el-table-column type="expand">
           <template slot-scope="props">
             <el-form inline class="table-expand">
@@ -84,21 +150,27 @@
         <el-table-column label="限重" prop="limitWeight"></el-table-column>
         <el-table-column label="最晚進廠日" prop="lastFactoryDate"></el-table-column>
         <el-table-column label="售價" prop="price"></el-table-column>
-        <el-table-column label="狀態" >
+        <el-table-column label="狀態">
           <template slot-scope="scope">
             <el-tag
               :type="scope.row.status ? 'primary' : 'danger'"
               disable-transitions
             >{{scope.row.status ? "開放中" : "已關閉"}}</el-tag>
-          </template></el-table-column>
+          </template>
+        </el-table-column>
         <el-table-column>
           <template slot-scope="scope">
-            <el-button size="mini" @click="showSaleOverlay(scope.$index)">編輯</el-button>
+            <el-button
+              icon="el-icon-edit"
+              type="info"
+              circle
+              @click="showSaleOverlay(scope.$index)"
+            ></el-button>
           </template>
         </el-table-column>
       </el-table>
       <el-dialog title="火箭編輯" :visible.sync="RisActive">
-        <el-form :model="selectedlist">
+        <el-form :model="selectedlist" :label-position="right" label-width="90px">
           <el-form-item label="火箭名稱：">
             <el-input v-model="selectedlist.rocketName" autocomplete="off" clearable></el-input>
           </el-form-item>
@@ -133,11 +205,11 @@
         </el-form>
         <div slot="footer" class="dialog-footer">
           <el-button @click="changeOverlay">取消</el-button>
-          <el-button type="primary" @click="modifyR">儲存</el-button>
+          <el-button type="primary" @click="modify_R(selectedlist)">儲存</el-button>
         </div>
       </el-dialog>
       <el-dialog title="機位編輯" :visible.sync="SisActive">
-        <el-form :model="selectedSalelist">
+        <el-form :model="selectedSalelist" :label-position="right" label-width="90px">
           <el-form-item label="火箭名稱：">
             <el-input v-model="selectedSalelist.missionId" autocomplete="off" clearable></el-input>
           </el-form-item>
@@ -160,7 +232,7 @@
         </el-form>
         <div slot="footer" class="dialog-footer">
           <el-button @click="changeSaleOverlay">取消</el-button>
-          <el-button type="primary" @click="modifyS">儲存</el-button>
+          <el-button type="primary" @click="modify_S(selectedSalelist)">儲存</el-button>
         </div>
       </el-dialog>
     </template>
@@ -181,7 +253,7 @@ export default {
           name: "福衛66號",
           weight: "10Kg",
           height: "LEO",
-          inclination: "25~34度",
+          inclination: "25~34",
           launchDate: "2020/08/01",
           toFactoryDate: "2019/12/20",
           budget: 1200,
@@ -193,7 +265,7 @@ export default {
           name: "ComeOnNASA",
           weight: "15Kg",
           height: "LEO",
-          inclination: "25~34度",
+          inclination: "25~34",
           launchDate: "2020/10/21",
           toFactoryDate: "2019/12/20",
           budget: 1200,
@@ -205,7 +277,7 @@ export default {
           name: "Super Rocket",
           weight: "1000Kg",
           height: "LEO",
-          inclination: "25~34度",
+          inclination: "25~34",
           launchDate: "2020/11/25",
           toFactoryDate: "2019/12/20",
           budget: 1200,
@@ -217,7 +289,7 @@ export default {
           name: "ComeOnNASA2",
           weight: "500Kg",
           height: "LEO",
-          inclination: "25~34度",
+          inclination: "25~34",
           launchDate: "2025/11/11",
           toFactoryDate: "2019/12/20",
           budget: 1200,
@@ -229,7 +301,7 @@ export default {
           name: "TKU",
           weight: "1000Kg",
           height: "LEO",
-          inclination: "25~34度",
+          inclination: "25~34",
           launchDate: "2030/12/31",
           toFactoryDate: "2019/12/20",
           budget: 1200,
@@ -241,7 +313,7 @@ export default {
           name: "福衛66號",
           weight: "10Kg",
           height: "LEO",
-          inclination: "25~34度",
+          inclination: "25~34",
           launchDate: "2020/08/01",
           toFactoryDate: "2019/12/20",
           budget: 1200,
@@ -253,7 +325,7 @@ export default {
           name: "ComeOnNASA",
           weight: "15Kg",
           height: "LEO",
-          inclination: "25~34度",
+          inclination: "25~34",
           launchDate: "2020/10/21",
           toFactoryDate: "2019/12/20",
           budget: 1200,
@@ -265,7 +337,7 @@ export default {
           name: "Super Rocket",
           weight: "1000Kg",
           height: "LEO",
-          inclination: "25~34度",
+          inclination: "25~34",
           launchDate: "2020/11/25",
           toFactoryDate: "2019/12/20",
           budget: 1200,
@@ -277,7 +349,7 @@ export default {
           name: "ComeOnNASA2",
           weight: "500Kg",
           height: "LEO",
-          inclination: "25~34度",
+          inclination: "25~34",
           launchDate: "2025/11/11",
           toFactoryDate: "2019/12/20",
           budget: 1200,
@@ -289,7 +361,7 @@ export default {
           name: "TKU",
           weight: "1000Kg",
           height: "LEO",
-          inclination: "25~34度",
+          inclination: "25~34",
           launchDate: "2030/12/31",
           toFactoryDate: "2019/12/20",
           budget: 1200,
@@ -327,16 +399,20 @@ export default {
       ],
       showrocketData: [],
       showsaleData: [],
+      showneedData: [],
       selected: -1,
       selectedlist: {},
       selectedSalelist: {},
+      selectedNeedlist: {},
       RisActive: false,
-      SisActive: false
+      SisActive: false,
+      NisActive: false,
     };
   },
   created() {
     this.setSrocket(this.rocketData);
     this.setSsale(this.saleData);
+    this.setNsale(this.needData);
   },
   methods: {
     setSrocket(arr) {
@@ -344,7 +420,6 @@ export default {
     },
     showOverlay(index) {
       this.selected = index;
-      this.selectedlist = {};
       this.selectedlist = JSON.parse(JSON.stringify(this.rocketData[index]));
       this.changeOverlay();
     },
@@ -352,11 +427,6 @@ export default {
       this.RisActive = !this.RisActive;
     },
     //點選儲存
-    modifyR: function() {
-      if (this.RisActive) {
-        this.modify_R(this.selectedlist);
-      }
-    },
     modify_R(arr) {
       if (this.selected > -1) {
         Vue.set(this.rocketData, this.selected, arr);
@@ -380,11 +450,6 @@ export default {
       this.SisActive = !this.SisActive;
     },
     // 點選儲存
-    modifyS: function() {
-      if (this.SisActive) {
-        this.modify_S(this.selectedSalelist);
-      }
-    },
     modify_S(arr) {
       if (this.selected > -1) {
         Vue.set(this.saleData, this.selected, arr);
@@ -394,6 +459,34 @@ export default {
       }
       this.setSsale(this.saleData);
       this.changeSaleOverlay();
+    },
+
+    setNsale(arr) {
+      this.showneedData = JSON.parse(JSON.stringify(arr));
+    },
+    showNeedOverlay(index) {
+      this.selected = index;
+      this.selectedNeedlist = JSON.parse(JSON.stringify(this.needData[index]));
+      this.changeNeedOverlay();
+    },
+    changeNeedOverlay() {
+      this.NisActive = !this.NisActive;
+    },
+    // 點選儲存
+    modify_N(arr) {
+      if (this.selected > -1) {
+        Vue.set(this.needData, this.selected, arr);
+        this.selected = -1;
+      } else {
+        this.needData.push(arr);
+      }
+      this.setNsale(this.needData);
+      this.changeNeedOverlay();
+      this.$message({
+        message: "儲存成功！",
+        type: "success",
+        center: true
+      });
     }
   }
 };
