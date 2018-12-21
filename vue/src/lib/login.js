b6c199b74303146bdf9fd66c199950438f70d7a7
minghsu <leo1997515@gms.tku.edu.tw>
2018-12-21 23:10:35 +0800

    api update

--- vue/src/lib/login.js
+++ vue/src/lib/login.js
@@ -3,7 +3,7 @@ import { config } from '../config/'
 
 const API_HOST = config.API_HOST;
 
-export function postLogin(payload,setLogin,setAlert) {
+export function postLogin(payload,onSuccess,onFailed) {
     let uri = `${API_HOST}/client/login`;
     try {
         const res = request({
@@ -16,27 +16,13 @@ export function postLogin(payload,setLogin,setAlert) {
             auth:false,
         });
         res.then(function(response) {
-            console.log(response.data.code)
             let data = response.data.data
             switch(response.data.code){
                 case 200:
-                    localStorage.setItem('userProfile',JSON.stringify(data))
-                    localStorage.setItem('userInfo',JSON.stringify({
-                        isLoggedIn: true,
-                        userId: 1,
-                        fullname: data.name,
-                        permission: (data.is_launch_company) ? 'company' : 'user',
-                    }))
-                    setLogin();
+                    onSuccess(data);
                     break;
                 case 401:
-                    localStorage.setItem('alert',JSON.stringify({
-                        hook: true,
-                        status: 'fail',
-                        title: '登入失敗：',
-                        message: '帳號或密碼錯誤，請重新輸入',
-                    }))
-                    setAlert();
+                    onFailed();
                     break;
             }
         })
