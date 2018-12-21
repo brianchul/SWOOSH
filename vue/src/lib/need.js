04a1cd20c5558ddf6ace9650ff930f5454a8e262
minghsu <leo1997515@gms.tku.edu.tw>
2018-12-22 00:51:58 +0800

    update need api

---
+++ vue/src/lib/need.js
@@ -0,0 +1,30 @@
+import { request } from './api'
+import { config } from '../config/'
+
+const API_HOST = config.API_HOST;
+
+export function postNeed(payload,onSuccess,onFailed) {
+    let uri = `${API_HOST}/clientOrder/add`;
+    try {
+        const res = request({
+            uri,
+            method: 'POST',
+            data: payload,
+            auth:false,
+        });
+        res.then(function(response) {
+            let data = response.data.data
+            console.log(response);
+            switch(response.data.code){
+                case 200:
+                    onSuccess(data);
+                    break;
+                case 400:
+                    onFailed();
+                    break;
+            }
+        })
+    } catch (e) {
+        console.log(e);
+    }
+}
